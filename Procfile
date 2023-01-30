1b3c65165b0f1291b24fcdb2968fa927129e8b10
yusame <yu.starts.yu@gmail.com>
2023-01-30 13:39:54 +0900

    Railwayへデプロイするための設定

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn lacrosse.wsgi --log-file -
--- lacrosse/settings.py
+++ lacrosse/settings.py
@@ -30,7 +30,7 @@ MEDIA_URL = '/media/'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
